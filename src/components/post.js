fd97cc27a2a7412661078469f558a14503337b11
Andy Mardell <andy@sidigital.co>
2019-07-19 23:38:45 +0100

    Typography

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import '../css/components/post.styl'
 
 const Post = ({ data, archive }) => (
   <div className='post'>
